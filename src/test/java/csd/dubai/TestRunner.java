0e374239f6401b70c756e9dc944e1253ea509119
Naveen Singh <naveen.singh@leanpitch.com>
2017-05-21 21:09:53 +0400

    Some changes

--- src/test/java/csd/dubai/TestRunner.java
+++ src/test/java/csd/dubai/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources")
+@CucumberOptions(features="src/test/resources/jobposting.feature")
 public class TestRunner {
 
 }
